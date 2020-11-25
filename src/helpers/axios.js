25684851e823af85f53f30b3feccd7362b210b44
Paramesh Krishna <parameshkrishna98@gmail.com>
2020-11-25 11:50:21 +0530

    changes done

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/",
-  // baseURL: "https://instagram-clone-backend98.herokuapp.com/",
+  // baseURL: "http://localhost:8080/",
+  baseURL: "https://instagram-clone-backend98.herokuapp.com/",
 });
 
 export default AxiosInstance;
